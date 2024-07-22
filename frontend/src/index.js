1023e53936959d02e997aa458108a28f3c53a27f
Annie Huang <annie.hn@gmail.com>
2024-07-22 21:46:18 +1000

    Create <ApolloProvider> in the index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
+export const link = createHttpLink({
+  uri: 'http://localhost:4000/',
+});
+
+export const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
