6f88146271907432ed723297a94d72c76338520d
Annie Huang <annie.hn@gmail.com>
2024-07-22 21:56:18 +1000

    use useQuery in App.js to get all the dogs like the sandbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,22 @@
 import './App.css';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_ALL_DOGS = gql`
+  query GetAllDogs {
+    getAllDogs {
+      name
+      age
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_ALL_DOGS);
+
+  if (loading) return null;
+  if (error) return 'Error: ' + error;
+  console.log(data);
+
   return (
     <div className='App'>
       <header className='App-header'>
